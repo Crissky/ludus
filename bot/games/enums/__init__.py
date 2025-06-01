4a683bbd34a76a05a4ab978f24d4cd02bffcfd85
Crissky <pg2006pe@hotmail.com>
2025-06-01 04:07:44 -0300

    Adiciona init e imports

---
+++ bot/games/enums/__init__.py
@@ -0,0 +1,7 @@
+from bot.games.enums.card import RoyalSuits
+from bot.games.enums.card import SpanishSuits
+from bot.games.enums.card import ColorSuits
+from bot.games.enums.card import ElementalSuits
+from bot.games.enums.card import RoyalNames
+from bot.games.enums.card import SpanishNames
+from bot.games.enums.card import StrippedSpanishNames
