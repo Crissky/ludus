4fa3e649785c6f67c1dfef824b9a6edd8fb1261a
Crissky <pg2006pe@hotmail.com>
2025-06-16 20:16:54 -0300

    Adiciona dunder methods

--- bot/games/player.py
+++ bot/games/player.py
@@ -1,7 +1,21 @@
 class Player:
     def __init__(self, id: str, name: str):
-        self.id = id
+        self.id = str(id)
         self.name = name
+
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.id == other.id
+        return False
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'Player(id={self.id}, name={self.name})'
 
     @property
     def user_id(self):
