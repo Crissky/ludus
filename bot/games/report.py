cc80b915bd038ad7833447900fd0d9e783d0472f
Crissky <pg2006pe@hotmail.com>
2025-06-17 20:53:54 -0300

    Adiciona str e repr e novos args no init

--- bot/games/report.py
+++ bot/games/report.py
@@ -3,7 +3,20 @@ from bot.games.player import Player
 
 
 class Report:
-    def __init__(self, game_id, player_list: List[Player]):
+    def __init__(
+        self,
+        game_id: int,
+        player: Player,
+        action: str,
+        turn: int,
+    ):
         self.game_id = game_id
-        self.player_list = player_list
-        self.report = {}
+        self.player = player
+        self.action = action
+        self.turn = turn
+
+    def __str__(self):
+        return f'{self.game_id}({self.turn}) - {self.player}: {self.action}'
+
+    def __repr__(self):
+        return f'Report({self})'
