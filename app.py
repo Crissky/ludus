f825fde99056707a7dca531e8211bd8abba9071a
Crissky <pg2006pe@hotmail.com>
2025-06-18 20:05:29 -0300

    WIP: Adiciona app

---
+++ app.py
@@ -0,0 +1,37 @@
+'''Arquivo principal que executa o telegram-bot.
+'''
+
+from datetime import datetime, time, timedelta
+from decouple import config
+
+from telegram.ext import Application
+
+TELEGRAM_TOKEN = config("TELEGRAM_TOKEN")
+MY_GROUP_ID = config('MY_GROUP_ID', cast=int)
+(
+    DEFAULT_GROUP,
+    CHAT_XP_GROUP,
+    WORDGAME_GROUP,
+) = range(3)
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    # Add Single Handler
+    # application.add_handler()
+
+    # Add Multiple Handlers
+    # application.add_handlers()
+
+    # Add Jobs
+    application.job_queue.run_repeating()
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
