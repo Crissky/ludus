7355a2d9922209d7670f684be1a91c715aaa8eff
Crissky <pg2006pe@hotmail.com>
2025-06-18 20:32:06 -0300

    Adiciona BasePlayButton e button functions

---
+++ bot/functions/buttons.py
@@ -0,0 +1,142 @@
+from random import choice
+from typing import List
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot.functions.enums.emoji import EmojiEnum, FaceEmojiEnum
+
+
+CALLBACK_CLOSE = '$close'
+LEFT_CLOSE_BUTTON_TEXT = f'{EmojiEnum.CLOSE.value}Fechar'
+RIGHT_CLOSE_BUTTON_TEXT = f'Fechar{EmojiEnum.CLOSE.value}'
+REFRESH_BUTTON_TEXT = f'{EmojiEnum.REFRESH.value}Atualizar'
+DETAIL_BUTTON_TEXT = f'{EmojiEnum.DETAIL.value}Detalhar'
+CALLBACK_KEY_LIST = [
+    'game_id'
+]
+
+
+# CALLBACK FUNCTIONS
+def callback_data_to_string(callback_data: dict) -> str:
+    '''Transforma um dicionário em uma string compactada usada no campo data
+    de um botão.
+    '''
+
+    items = []
+    for key, value in callback_data.items():
+        key_int = CALLBACK_KEY_LIST.index(key)
+        if isinstance(value, str):
+            items.append(f'{key_int}:"{value}"')
+        else:
+            items.append(f'{key_int}:{value}')
+    text = ','.join(items)
+    text = f'{{{text}}}'
+
+    return text
+
+
+def callback_data_to_dict(callback_data_str: str) -> dict:
+    '''Transforma de volta uma string compactada usada no campo data
+    de um botão em um dicionário.
+    '''
+
+    callback_data = eval(callback_data_str)
+    callback_data = {
+        CALLBACK_KEY_LIST[key]: value
+        for key, value in callback_data.items()
+    }
+    return callback_data
+
+
+# BUTTONS FUNCTIONS
+def get_close_button(
+    user_id: int,
+    text: str = None,
+    right_icon: bool = False,
+) -> InlineKeyboardButton:
+    '''Se user_id for None, qualquer um pode fechar a mensagem,
+    caso contrário, somente o usuário com o mesmo user_id poderar fechar
+    a mensagem.
+    '''
+
+    if text is None:
+        text = LEFT_CLOSE_BUTTON_TEXT
+        if right_icon:
+            text = RIGHT_CLOSE_BUTTON_TEXT
+
+    return InlineKeyboardButton(
+        text=text,
+        callback_data=(
+            f'{{"command":"{CALLBACK_CLOSE}",'
+            f'"user_id":{user_id}}}'
+        )
+    )
+
+
+def get_refresh_close_button(
+    user_id: int,
+    refresh_data: str = 'refresh',
+    to_detail: bool = False,
+) -> List[InlineKeyboardButton]:
+    '''Se user_id for None, qualquer um pode fechar a mensagem,
+    caso contrário, somente o usuário com o mesmo user_id poderar fechar
+    a mensagem.
+    '''
+
+    button_list = []
+    button_list.append(
+        InlineKeyboardButton(
+            REFRESH_BUTTON_TEXT,
+            callback_data=(
+                f'{{"{refresh_data}":1,'
+                f'"user_id":{user_id}}}'
+            )
+        )
+    )
+    if to_detail:
+        button_list.append(
+            InlineKeyboardButton(
+                DETAIL_BUTTON_TEXT,
+                callback_data=(
+                    f'{{"{refresh_data}":1,"verbose":"v",'
+                    f'"user_id":{user_id}}}'
+                )
+            )
+        )
+    button_list.append(get_close_button(user_id=user_id, right_icon=True))
+
+    return button_list
+
+
+def get_random_refresh_text() -> str:
+    emoji = choice(list(FaceEmojiEnum)).value
+    return f'Atualizado{emoji}'
+
+
+def get_close_keyboard(user_id: int) -> InlineKeyboardMarkup:
+    '''Se user_id for None, qualquer um pode fechar a mensagem,
+    caso contrário, somente o usuário com o mesmo user_id poderar fechar
+    a mensagem.
+    '''
+
+    return InlineKeyboardMarkup([[
+        get_close_button(user_id=user_id)
+    ]])
+
+
+def get_refresh_close_keyboard(
+    user_id: int,
+    refresh_data: str = 'refresh',
+    to_detail: bool = False,
+) -> InlineKeyboardMarkup:
+    '''Se user_id for None, qualquer um pode fechar a mensagem,
+    caso contrário, somente o usuário com o mesmo user_id poderar fechar
+    a mensagem.
+    '''
+
+    return InlineKeyboardMarkup([
+        get_refresh_close_button(
+            user_id=user_id,
+            refresh_data=refresh_data,
+            to_detail=to_detail
+        )
+    ])
---
+++ bot/games/buttons/play_button.py
@@ -0,0 +1,31 @@
+from bot.functions.buttons import (
+    callback_data_to_dict,
+    callback_data_to_string
+)
+from bot.games.boards.board import BaseBoard
+
+
+from telegram import InlineKeyboardButton
+
+
+class BasePlayButton:
+    def __init__(self, game: BaseBoard, text: str, **callback_data):
+        self.game = game
+        self.text = text
+        self.callback_data = callback_data
+
+    def data_to_str(self) -> str:
+        data = {
+            'game_id': self.game.id,
+            **self.callback_data
+        }
+
+        return callback_data_to_string(data)
+
+    def str_to_data(self, data: str):
+        return callback_data_to_dict(data)
+
+    def make_button(self) -> InlineKeyboardButton:
+        callback_data = self.data_to_str()
+        text = self.text
+        return InlineKeyboardButton(text=text, callback_data=callback_data)
