823bd2002fc821e28295a86ce0c8e777656f7415
Crissky <pg2006pe@hotmail.com>
2025-06-18 21:25:38 -0300

    Adiciona get e set item, iter, str e repr

--- bot/games/hands/hand.py
+++ bot/games/hands/hand.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import List, Union
+from typing import Generator, List, Union
 from bot.games.cards.card import Card
 
 
@@ -8,8 +8,23 @@ class BaseHand:
         self.max_size = max_size
         self.card_list = list(cards)
 
+    def __getitem__(self, index: int) -> Card:
+        return self.card_list[index]
+
+    def __setitem__(self, index: int, value: Card):
+        self.card_list[index] = value
+
+    def __iter__(self) -> Generator[Card]:
+        yield from self.card_list
+
     def __len__(self):
         return len(self.card_list)
+
+    def __str__(self):
+        return ', '.join((card.text for card in self.card_list))
+
+    def __repr__(self):
+        return f'Hand({self.card_list})'
 
     def add_card(
         self,
