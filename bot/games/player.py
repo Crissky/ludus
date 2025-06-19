ad2e918eb511c8a3115c6fdbf5cb0f92bc7fb8a8
Crissky <pg2006pe@hotmail.com>
2025-06-19 03:27:52 -0300

    Corrige import e refatora id

--- bot/games/player.py
+++ bot/games/player.py
@@ -1,11 +1,11 @@
-from typing import Generator
+from collections.abc import Generator
 from bot.games.cards.card import Card
 from bot.games.hands.hand import BaseHand
 
 
 class Player:
-    def __init__(self, id: str, name: str, hand: BaseHand = None):
-        self.id = str(id)
+    def __init__(self, player_id: str, name: str, hand: BaseHand = None):
+        self.id = str(player_id)
         self.name = name
         if hand is None:
             hand = BaseHand()
@@ -19,7 +19,7 @@ class Player:
     def __hash__(self):
         return hash(self.id)
 
-    def __init__(self) -> Generator[Card]:
+    def __iter__(self) -> Generator[Card]:
         yield from self.hand
 
     def __getitem__(self, index):
