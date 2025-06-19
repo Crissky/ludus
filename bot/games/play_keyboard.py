7add10fe3ee44ae1a85ec750661ec2bec266662a
Crissky <pg2006pe@hotmail.com>
2025-06-18 21:28:49 -0300

    Refatora número de botões por linha

--- bot/games/play_keyboard.py
+++ bot/games/play_keyboard.py
@@ -5,14 +5,14 @@ from bot.games.buttons.play_button import BasePlayButton
 
 
 class PlayKeyBoard:
-    def __init__(self, col_per_row: int = 1, *play_buttons: BasePlayButton):
-        self.col_per_row = col_per_row
-        self.play_buttons = play_buttons
+    def __init__(self, buttons_per_row: int = 1, *play_buttons: BasePlayButton):
+        self.buttons_per_row = buttons_per_row
+        self.play_buttons = list(play_buttons)
 
     def get_keyboard(self) -> List[List[InlineKeyboardButton]]:
         keyboard = []
-        for i in range(0, len(self.play_buttons), self.col_per_row):
-            row = self.play_buttons[i:i+self.col_per_row]
+        for i in range(0, len(self.play_buttons), self.buttons_per_row):
+            row = self.play_buttons[i:i+self.buttons_per_row]
             keyboard.append([button.make_button() for button in row])
 
         return keyboard
