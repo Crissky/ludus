fd6715444c4ad6e7cd3ffc7dd3d6487342b24efd
Crissky <pg2006pe@hotmail.com>
2025-06-23 20:28:43 -0300

    Refatora funções de listagem de boards

--- bot/games/boards/__init__.py
+++ bot/games/boards/__init__.py
@@ -2,13 +2,36 @@ from bot.games.boards.board import BaseBoard
 from bot.games.boards.colorgame_board import ColorGameBoard
 
 
-BOARD_LIST = [
-    ColorGameBoard,
-]
+def get_solo_board_list() -> list[BaseBoard]:
+    solo_board_list = []
+
+    return solo_board_list
+
+
+def get_duel_board_list() -> list[BaseBoard]:
+    duel_board_list = []
+
+    return duel_board_list
+
+
+def get_party_board_list() -> list[BaseBoard]:
+    party_board_list = [
+        ColorGameBoard,
+    ]
+
+    return party_board_list
+
+
+def get_board_list() -> list[BaseBoard]:
+    board_list = get_solo_board_list()
+    board_list.extend(get_duel_board_list())
+    board_list.extend(get_party_board_list())
+
+    return sorted(board_list, key=lambda board: board.__name__)
 
 
 def board_factory(board_name: str) -> BaseBoard:
-    for board_class in BOARD_LIST:
+    for board_class in get_board_list():
         if board_class.__name__ == board_name:
             return board_class
     raise ValueError(f'Board "{board_name}" não encontrada.')
