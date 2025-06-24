b8b5fec31a93ae287c1a6b4253a3d4b48766fa87
Crissky <pg2006pe@hotmail.com>
2025-06-24 00:23:47 -0300

    Refatora keyboard para também agregar linhas por grupos

--- bot/games/play_keyboard.py
+++ bot/games/play_keyboard.py
@@ -16,26 +16,39 @@ class PlayKeyBoard:
             )
 
         self.buttons_per_row = buttons_per_row
-        self.play_buttons: List[BasePlayButton] = []
+        self.play_button_list: List[BasePlayButton] = []
 
         for play_button in play_buttons:
             self.add_button(play_button)
 
     def add_button(self, button: BasePlayButton):
-        if button in self.play_buttons:
+        if button in self.play_button_list:
             raise ValueError('Já existe um botão com esse nome.')
         if not isinstance(button, BasePlayButton):
             raise ValueError(
                 'O botão deve ser uma instância de BasePlayButton.'
             )
-        self.play_buttons.append(button)
+        self.play_button_list.append(button)
 
     @property
     def buttons(self) -> List[List[InlineKeyboardButton]]:
         button_lists = []
-        for i in range(0, len(self.play_buttons), self.buttons_per_row):
-            row = self.play_buttons[i:i+self.buttons_per_row]
-            button_lists.append([button.make_button() for button in row])
+        sorted_play_button_list = sorted(
+            self.play_button_list,
+            key=lambda x: x.group
+        )
+
+        i = 0
+        group = 0
+        for play_button in sorted_play_button_list:
+            if i % self.buttons_per_row == 0 or play_button.group > group:
+                i = 0
+                group = play_button.group
+                row = []
+                button_lists.append(row)
+
+            row.append(play_button.make_button())
+            i += 1
 
         return button_lists
 
