764450e54008a2b8eda1ae6ade2caad0f5e544d1
Crissky <pg2006pe@hotmail.com>
2025-06-26 01:07:31 -0300

    Adiciona is_playable_card

--- bot/games/boards/colorgame_board.py
+++ bot/games/boards/colorgame_board.py
@@ -1,4 +1,5 @@
 from bot.games.boards.cardgame_board import BaseCardGameBoard
+from bot.games.cards.card import Card
 from bot.games.decks.color import ColorDeck
 from bot.games.player import Player
 
@@ -16,3 +17,26 @@ class ColorsGameBoard(BaseCardGameBoard):
             initial_hand_size=7,
             hand_kwargs={}
         )
+        self.pending_draw = 0
+
+    def is_playable_card(self, card: Card) -> bool:
+        if not self.discard_piles:
+            return True
+
+        top_card = self.discard_piles[0].peek()
+
+        # Testa o empilhamento de carta PLUS
+        if self.pending_draw > 0:
+            if card.plus_value > 0 and card.plus_value >= top_card.plus_value:
+                return True
+            return False
+
+        # Caso normal: tem que bater cor ou nome
+        if (
+            card.suit == top_card.suit or
+            card.name == top_card.name or
+            card.is_wild
+        ):
+            return True
+
+        return False
