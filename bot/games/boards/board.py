043bd9976f657d4d3e09f441cc3a1ecd71b3675b
Crissky <pg2006pe@hotmail.com>
2025-07-04 20:08:39 -0300

    Atualiza __repr__

--- bot/games/boards/board.py
+++ bot/games/boards/board.py
@@ -69,7 +69,11 @@ class BaseBoard(ABC):
         return text
 
     def __repr__(self) -> str:
-        return f'Board({self.DISPLAY_NAME})'
+        return (
+            f'{self.__class__.__name__}('
+            f'id={self.id}, '
+            f'name={self.DISPLAY_NAME})'
+        )
 
     def add_player(self, player: Player):
         if not isinstance(player, Player):
