3f4c34df1d770e130504cd5538507ba5b2604f8f
Crissky <pg2006pe@hotmail.com>
2025-07-05 02:21:07 -0300

    WIP: Adicionando play

--- bot/games/boards/colorgame_board.py
+++ bot/games/boards/colorgame_board.py
@@ -1,6 +1,8 @@
 from bot.games.boards.cardgame_board import BaseCardGameBoard
 from bot.games.cards.card import Card
 from bot.games.decks.color import ColorDeck
+from bot.games.enums.command import CallbackKeyEnum, CommandEnum
+from bot.games.play_keyboard import PlayKeyBoard
 from bot.games.player import Player
 
 
@@ -20,6 +22,42 @@ class ColorsGameBoard(BaseCardGameBoard):
             max_total_players=4,
         )
         self.pending_draw = 0
+        self.selecting_color = False
+
+    def player_keyboard(self, player: Player) -> PlayKeyBoard:
+        if self.selecting_color is False or self.is_started is not True:
+            return super().player_keyboard(player)
+
+    def play(self, player: Player, play_dict: dict):
+        command_str = play_dict[CallbackKeyEnum.COMMAND]
+        command = CommandEnum[command_str]
+        game_id = play_dict[CallbackKeyEnum.GAME_ID]
+        hand_position = play_dict.get(CallbackKeyEnum.HAND_POSITION)
+        selected_color = play_dict.get(CallbackKeyEnum.SELECTED_COLOR)
+
+        if game_id != self.id:
+            action = f'Jogo inválido: {game_id}'
+            return self.add_log(player=False, action=action)
+        if player != self.current_player:
+            action = f'Não é a vez de {player}.'
+            return self.add_log(player=False, action=action)
+        if not self.player_in_game(player):
+            action = f'{player} não está mais no jogo.'
+            return self.add_log(player=False, action=action)
+
+        player = self.get_player(player)
+        card = player.get_card(hand_position)
+
+        if not isinstance(card, Card):
+            action = f'Carta na posição {hand_position} não encontrada.'
+            return self.add_log(player=False, action=action)
+
+        if command == CommandEnum.PLAY:
+            ...
+        elif command == CommandEnum.DRAW:
+            ...
+        elif command == CommandEnum.PASS:
+            ...
 
     def is_playable_card(self, card: Card) -> bool:
         if not self.discard_piles:
