40fe03ecf52ecfdc4ea503fca203d921faa9635c
Crissky <pg2006pe@hotmail.com>
2025-07-22 22:30:23 -0300

    Adiciona tipos para as cartas

--- bot/games/cards/scoundrel_card.py
+++ bot/games/cards/scoundrel_card.py
@@ -1,5 +1,5 @@
 from bot.games.cards.card import Card
-from bot.games.enums.card import RoyalNames
+from bot.games.enums.card import RoyalNames, RoyalSuits
 
 
 class ScoundrelCard(Card):
@@ -13,3 +13,15 @@ class ScoundrelCard(Card):
     @property
     def suit_value(self):
         return 1
+
+    @property
+    def is_weapon(self):
+        return self.suit in [RoyalSuits.DIAMONDS]
+
+    @property
+    def is_potion(self):
+        return self.suit in [RoyalSuits.HEARTS]
+
+    @property
+    def is_enemy(self):
+        return self.suit in [RoyalSuits.CLUBS, RoyalSuits.SPADES]
