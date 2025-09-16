aa0c5e913c4aba28aa04fb8416b2ab93d42eee3a
Crissky <pg2006pe@hotmail.com>
2025-09-15 22:28:14 -0300

    Refatora nome de arquivo e adiciona init

---
+++ bot/games/boards/golf_solitaire_board.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from bot.games.boards.cardgame_board import BaseCardGameBoard
+from bot.games.cards.card import Card
+from bot.games.decks.royal import RoyalDeck
+from bot.games.player import Player
+
+
+class GolfSolitaireBoard(BaseCardGameBoard):
+    DISPLAY_NAME: str = '🏌️‍♂️Golf Solitaire'
+    DESCRIPTION: str = ('')
+
+    def __init__(self, *players: Player, debug: bool = False):
+        draw_pile = RoyalDeck(is_shuffle=True)
+        super().__init__(
+            draw_pile,
+            *players,
+            total_discard_pile=9,
+            discard_at_start=True,
+            initial_hand_size=0,
+            hand_kwargs={'max_size': 0},
+            min_total_players=1,
+            max_total_players=1,
+            debug=debug,
+        )
+        self.enemy = Player(player_id='0000000000', name='Carcereiro')
+
+        self.debug_attr_list.extend([])
+
+    # ABSTRACT METHODS #######################################################
+    def play(self, player: Player, play_dict: dict):
+        return super().play(player=player, play_dict=play_dict)
+
+    def is_playable_card(self, card: Card) -> bool:
+        ...
+
+    def winners(self) -> List[Player]:
+        ...
