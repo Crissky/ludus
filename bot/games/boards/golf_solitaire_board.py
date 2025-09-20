eb342970c6f94a15ab612d949aaaa52818d28fec
Crissky <pg2006pe@hotmail.com>
2025-09-19 22:15:00 -0300

    Adiciona create_board

--- bot/games/boards/golf_solitaire_board.py
+++ bot/games/boards/golf_solitaire_board.py
@@ -26,10 +26,21 @@ class GolfSolitaireBoard(BaseCardGameBoard):
         )
         self.enemy = Player(player_id='0000000000', name='Solitaire')
         self.board = []
+        self.create_board()
 
         self.debug_attr_list.extend([
             'board',
         ])
+
+    def create_board(self):
+        num_rows = 5
+        num_card_per_row = 7
+        for _ in range(num_rows):
+            row = []
+            for _ in range(num_card_per_row):
+                card_list = self.draw()
+                row.extend(card_list)
+            self.board.extend(row)
 
     # ABSTRACT METHODS #######################################################
     def play(self, player: Player, play_dict: dict):
