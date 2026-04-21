b8089382da05596487450150ee261d1b117ec96f
Crissky <pg2006pe@hotmail.com>
2026-04-21 15:34:25 -0300

    Adiciona TerritoryEnum

---
+++ bot/games/enums/warfare.py
@@ -0,0 +1,56 @@
+from enum import Enum
+
+
+class TerritoryEnum:
+    # América do Sul
+    BRASIL = "Brasil"
+    ARGENTINA = "Argentina"
+    PERU = "Peru"
+    VENEZUELA = "Venezuela"
+
+    # América do Norte
+    MEXICO = "México"
+    CALIFORNIA = "Califórnia"
+    NOVA_YORK = "Nova York"
+    ALASCA = "Alasca"
+    OTTAWA = "Ottawa"
+    LABRADOR = "Labrador"
+    GROENLANDIA = "Groenlândia"
+    VANCOUVER = "Vancouver"
+    MACKENZIE = "Mackenzie"
+
+    # Europa
+    INGLATERRA = "Inglaterra"
+    FRANCA = "França"
+    ALEMANHA = "Alemanha"
+    ITALIA = "Itália"
+    POLONIA = "Polônia"
+    SUECIA = "Suécia"
+
+    # África
+    ARGELIA = "Argélia"
+    EGITO = "Egito"
+    SUDAO = "Sudão"
+    CONGO = "Congo"
+    AFRICA_DO_SUL = "África do Sul"
+    MADAGASCAR = "Madagascar"
+
+    # Ásia
+    ORIENTE_MEDIO = "Oriente Médio"
+    INDIA = "Índia"
+    CHINA = "China"
+    JAPAO = "Japão"
+    MONGOLIA = "Mongólia"
+    VLADIVOSTOK = "Vladivostok"
+
+    OMSK = "Omsk"
+    DUDINKA = "Dudinka"
+    SIBERIA = "Sibéria"
+    TCHITA = "Tchita"
+    ARAL = "Aral"
+
+    # Oceania
+    AUSTRÁLIA = "Austrália"
+    NOVA_GUINE = "Nova Guiné"
+    BORNEO = "Borneo"
+    SUMATRA = "Sumatra"
