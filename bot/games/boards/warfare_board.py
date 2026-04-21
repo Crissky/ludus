6f96674de3d117f3d6194ede2ac55985a266367c
Crissky <pg2006pe@hotmail.com>
2026-04-21 13:45:32 -0300

    Adicionando warfare game

---
+++ bot/games/boards/warfare_board.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+
+from bot.games.player import Player
+
+
+@dataclass
+class territory:
+    name: str
+    occupier: Player = None
+    troops: int = 0
+
+
+@dataclass
+class continent:
+    name: str
+    majority_bonus: int
+    totality_bonus: int
+    territories: list = field(default_factory=list)
+
+    def __post_init__(self):
+        if self.totality_bonus <= self.majority_bonus:
+            raise ValueError(
+                "O bônus pela dominância total deve ser maior que o bônus "
+                "pela maioria dos territórios.\n"
+                f"BÔNUS DE MAIORIA: {self.majority_bonus}.\n"
+                f"BÔNUS TOTAL: {self.totality_bonus}."
+            )
+        if self.majority_bonus == 0 or self.totality_bonus == 0:
+            majority_text = ""
+            totality_text = ""
+            if self.majority_bonus == 0:
+                majority_text = f"BÔNUS DE MAIORIA: {self.majority_bonus}.\n"
+            if self.totality_bonus == 0:
+                totality_text = f"BÔNUS TOTAL: {self.totality_bonus}."
+
+            raise ValueError(
+                "Os bônus devem ser maiores que zero.\n{}{}".format(
+                    majority_text, totality_text
+                )
+            )
+
+    def __iter__(self):
+        yield from self.territories
+
+
+@dataclass
+class world:
+    continents: list = field(default_factory=list)
+
+    def __post_init__(self):
+        self.territories = []
+        for cont in self.continents:
+            self.territories.extend(cont.territories)
