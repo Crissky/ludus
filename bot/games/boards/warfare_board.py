61b5a0e27ab7e3882ae74c219bd3a14b4ab8ca58
Crissky <pg2006pe@hotmail.com>
2026-05-06 00:48:42 -0300

    Refatora WarfareBoard para herdar BaseCardGameBoard

--- bot/games/boards/warfare_board.py
+++ bot/games/boards/warfare_board.py
@@ -1,23 +1,27 @@
 import logging
 
-from bot.games.boards.board import BaseBoard
+from bot.games.boards.cardgame_board import BaseCardGameBoard
+from bot.games.decks.warfare import TerritoriesDeck
 from bot.games.player import Player
-
 
 logger = logging.getLogger(__name__)
 
 
-class WarfareBoard(BaseBoard):
-    def __init__(
-        self,
-        *players: Player,
-        min_total_players: int = 3,
-        max_total_players: int = 6,
-        debug: bool = False,
-    ):
+class WarfareBoard(BaseCardGameBoard):
+    DISPLAY_NAME: str = "🌎Warfare"
+    DESCRIPTION: str = "DESCRIÇÃO E REGRAS DO WARFARE PRECISAM SER DEFINIDAS."
+
+    def __init__(self, *players: Player, debug: bool = False):
+        draw_pile = TerritoriesDeck()
         super().__init__(
+            draw_pile,
             *players,
-            min_total_players=min_total_players,
-            max_total_players=max_total_players,
+            is_shuffle_deck=True,
+            total_discard_pile=1,
+            discard_at_start=True,
+            initial_hand_size=0,
+            hand_kwargs={},
+            min_total_players=3,
+            max_total_players=6,
             debug=debug,
         )
